5706b6b636b91f1fd61c15093e495385aef73c94
Alex <broodtech@live.de>
2023-12-28 15:48:27 +0100

    Updated test game and logging

--- ZweigDungeon.Application/Game.cs
+++ ZweigDungeon.Application/Game.cs
@@ -1,47 +1,44 @@
-﻿using ZweigDungeon.Common.Interfaces.Platform;
+﻿using System.Diagnostics;
+using ZweigDungeon.Common.Constants;
+using ZweigDungeon.Common.Interfaces.Platform;
 using ZweigDungeon.Common.Interfaces.Platform.Messages;
 using ZweigDungeon.Common.Interfaces.Video;
 using ZweigDungeon.Common.Services.Messages;
 
 namespace ZweigDungeon.Application;
 
-public class Game : IDisposable, IWindowListener
+public class Game : IDisposable, IWindowListener, IKeyboardListener
 {
 	private readonly IVideoContext           m_video;
 	private readonly IDisposable             m_subscription;
+	private readonly IDisposable             m_keyboard;
 	private readonly CancellationTokenSource m_cancellationTokenSource;
-	private          IVideoSurface?          m_testSurface;
 
 	public Game(MessageBus messageBus, IVideoContext video)
 	{
 		m_video                   = video;
 		m_subscription            = messageBus.Subscribe<IWindowListener>(this);
+		m_keyboard                = messageBus.Subscribe<IKeyboardListener>(this);
 		m_cancellationTokenSource = new CancellationTokenSource();
 	}
 
 	private void ReleaseUnmanagedResources()
 	{
 		m_cancellationTokenSource.Dispose();
+		m_subscription.Dispose();
+		m_keyboard.Dispose();
 	}
 
-	private void Dispose(bool disposing)
-	{
-		ReleaseUnmanagedResources();
-		if (disposing)
-		{
-			m_subscription.Dispose();
-		}
-	}
 
 	public void Dispose()
 	{
-		Dispose(true);
+		ReleaseUnmanagedResources();
 		GC.SuppressFinalize(this);
 	}
 
 	~Game()
 	{
-		Dispose(false);
+		ReleaseUnmanagedResources();
 	}
 
 	public void WindowCreated(IPlatformWindow window)
@@ -49,19 +46,10 @@ public class Game : IDisposable, IWindowListener
 		window.SetTitle("ZweigDungeon");
 		window.SetMinimumSize(640, 480);
 		window.Show();
-		m_video.CreateSurface(2, 2, out m_testSurface);
-		m_video.MapSurfaceData(m_testSurface, pixels =>
-		{
-			pixels[0] = new VideoColor { Red = 255, Green = 0, Blue = 0, Alpha = 255 };
-			pixels[1] = new VideoColor { Red = 0, Green = 255, Blue = 0, Alpha = 255 };
-			pixels[2] = new VideoColor { Red = 0, Green = 0, Blue = 255, Alpha = 255 };
-			pixels[3] = new VideoColor { Red = 255, Green = 255, Blue = 255, Alpha = 255 };
-		});
 	}
 
 	public void WindowClosing(IPlatformWindow window)
 	{
-		m_testSurface?.Dispose();
 		m_cancellationTokenSource.Cancel();
 	}
 
@@ -71,21 +59,26 @@ public class Game : IDisposable, IWindowListener
 		var height = window.GetViewportHeight();
 		m_video.BeginFrame(width, height);
 
-		if (m_testSurface != null)
-		{
-			var dst = new VideoRect { Left = 0, Top     = 0, Width  = 64, Height = 64 };
-			var src = new VideoRect { Left = 0, Top     = 0, Width  = 2, Height = 2 };
-			var col = new VideoColor { Red = 255, Green = 255, Blue = 255, Alpha   = 255 };
-
-			m_video.DrawSurface(m_testSurface, dst, src, col, VideoFlags.None);
-			dst.Left += 72;
-			m_video.DrawSurface(m_testSurface, dst, src, col, VideoFlags.MirrorHorizontal);
-			dst.Left += 72;
-			m_video.DrawSurface(m_testSurface, dst, src, col, VideoFlags.MirrorVertical);
-			dst.Left += 72;
-			m_video.DrawSurface(m_testSurface, dst, src, col, VideoFlags.MirrorHorizontal | VideoFlags.MirrorVertical);
-		}
-
 		m_video.FinishFrame();
+	}
+
+	public void KeyPressed(IPlatformKeyboard keyboard, KeyboardKey key)
+	{
+		if (Debugger.IsAttached)
+		{
+			Debug.WriteLine($"{nameof(KeyPressed)} {key}");
+		}
+	}
+
+	public void KeyReleased(IPlatformKeyboard keyboard, KeyboardKey key)
+	{
+		if (Debugger.IsAttached)
+		{
+			Debug.WriteLine($"{nameof(KeyReleased)} {key}");
+		}
+	}
+
+	public void KeyTyped(IPlatformKeyboard keyboard, char character)
+	{
 	}
 }
--- ZweigDungeon.Application/Program.cs
+++ ZweigDungeon.Application/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using ZweigDungeon.Common.Interfaces.Platform;
 using ZweigDungeon.Common.Interfaces.Video;
@@ -14,33 +15,43 @@ internal static class Program
 {
 	private static void Main(string[] args)
 	{
-		var serviceConfig = new ServiceConfiguration();
-		serviceConfig.AddSingleton<MessageBus>();
-		serviceConfig.AddSingleton<NativeLibraryLoader>();
-		serviceConfig.AddSingleton<IVideoContext, OpenGLContext>();
+		try
+		{
+			var serviceConfig = new ServiceConfiguration();
+			serviceConfig.AddSingleton<MessageBus>();
+			serviceConfig.AddSingleton<NativeLibraryLoader>();
+			serviceConfig.AddSingleton<IVideoContext, OpenGLContext>();
 
-		if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-		{
-			serviceConfig.AddSingleton<IPlatformWindow, Win32Window>();
-			serviceConfig.AddSingleton<IPlatformKeyboard, Win32Keyboard>();
-			serviceConfig.AddSingleton<IPlatformMouse, Win32Mouse>();
-			serviceConfig.AddSingleton<IPlatformAudio, Win32AudioDevice>();
-			serviceConfig.AddSingleton<IPlatformVideo, Win32OpenGLDevice>();
-		}
-		else
-		{
-			throw new NotSupportedException("Platform is not implemented");
-		}
-		
-
-		serviceConfig.AddSingleton<Game>();
-		using (var services = serviceConfig.Build())
-		{
-			var window = services.GetRequiredService<IPlatformWindow>();
-			window.Create();
-			while (window.IsAvailable())
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
 			{
-				window.Update();
+				serviceConfig.AddSingleton<IPlatformWindow, Win32Window>();
+				serviceConfig.AddSingleton<IPlatformKeyboard, Win32Keyboard>();
+				serviceConfig.AddSingleton<IPlatformMouse, Win32Mouse>();
+				serviceConfig.AddSingleton<IPlatformAudio, Win32AudioDevice>();
+				serviceConfig.AddSingleton<IPlatformVideo, Win32OpenGLDevice>();
+			}
+			else
+			{
+				throw new NotSupportedException("Platform is not implemented");
+			}
+
+
+			serviceConfig.AddSingleton<Game>();
+			using (var services = serviceConfig.Build())
+			{
+				var window = services.GetRequiredService<IPlatformWindow>();
+				window.Create();
+				while (window.IsAvailable())
+				{
+					window.Update();
+				}
+			}
+		}
+		catch(Exception ex)
+		{
+			if (Debugger.IsAttached)
+			{
+				Debug.WriteLine(ex);
 			}
 		}
 	}
