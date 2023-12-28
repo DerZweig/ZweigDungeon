97d52835374e77dbb8d4641fcded22f3338ab622
Alex <broodtech@live.de>
2023-12-28 01:13:14 +0100

    Fixed tga header

--- ZweigDungeon.Common/Assets/Images/TGA/Structures/TGAHeader.cs
+++ ZweigDungeon.Common/Assets/Images/TGA/Structures/TGAHeader.cs
@@ -3,19 +3,31 @@ using ZweigDungeon.Common.Assets.Images.TGA.Constants;
 
 namespace ZweigDungeon.Common.Assets.Images.TGA.Structures;
 
-[StructLayout(LayoutKind.Sequential)]
+[StructLayout(LayoutKind.Explicit)]
 internal struct TGAHeader
 {
-    public byte                    IdLength;
-    public byte                    ColourMapLength;
-    public TGAImageType            ImageType;
-    public short                   ColorMapFirst;
-    public short                   ColorMapCount;
-    public byte                    BitsPerEntry;
-    public short                   OriginLeft;
-    public short                   OriginTop;
-    public short                   SizeWidth;
-    public short                   SizeHeight;
-    public byte                    BitsPerPixel;
-    public TGAImageDescriptorFlags Descriptor;
+	[FieldOffset(0)]
+	public byte                    IdLength;
+	[FieldOffset(1)]
+	public byte                    ColorMapType;
+	[FieldOffset(2)]
+	public TGAImageType            ImageType;
+	[FieldOffset(3)]
+	public short                   ColorMapOrigin;
+	[FieldOffset(5)]
+	public short                   ColorMapLength;
+	[FieldOffset(7)]
+	public byte                    ColorMapDepth;
+	[FieldOffset(8)]
+	public short                   OriginLeft;
+	[FieldOffset(10)]
+	public short                   OriginTop;
+	[FieldOffset(12)]
+	public short                   SizeWidth;
+	[FieldOffset(14)]
+	public short                   SizeHeight;
+	[FieldOffset(16)]
+	public byte                    BitsPerPixel;
+	[FieldOffset(17)]
+	public TGAImageDescriptorFlags Descriptor;
 }
