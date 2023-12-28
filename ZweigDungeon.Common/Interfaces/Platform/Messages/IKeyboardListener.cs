db4bbc522afb89d49a43f15e04b34840049226e1
Alex <broodtech@live.de>
2023-12-28 15:48:02 +0100

    Keyboard and Mouse implementation

---
+++ ZweigDungeon.Common/Interfaces/Platform/Messages/IKeyboardListener.cs
@@ -0,0 +1,10 @@
+﻿using ZweigDungeon.Common.Constants;
+
+namespace ZweigDungeon.Common.Interfaces.Platform.Messages;
+
+public interface IKeyboardListener
+{
+	void KeyPressed(IPlatformKeyboard keyboard, KeyboardKey key);
+	void KeyReleased(IPlatformKeyboard keyboard, KeyboardKey key);
+	void KeyTyped(IPlatformKeyboard keyboard, char character);
+}
---
+++ ZweigDungeon.Common/Interfaces/Platform/Messages/IMouseListener.cs
@@ -0,0 +1,12 @@
+﻿using ZweigDungeon.Common.Constants;
+
+namespace ZweigDungeon.Common.Interfaces.Platform.Messages;
+
+public interface IMouseListener
+{
+	void MouseMoved(IPlatformMouse mouse, int left, int top);
+	void ButtonPressed(IPlatformMouse mouse, MouseButton button);
+	void ButtonReleased(IPlatformMouse mouse, MouseButton button);
+	void ScrolledHorizontal(IPlatformMouse mouse, int offset);
+	void ScrolledVertical(IPlatformMouse mouse, int offset);
+}
